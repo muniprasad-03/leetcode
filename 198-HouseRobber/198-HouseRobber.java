20f0ea411c266ee1e1d37072a8850ee93756c803
muniprasad-03 <muniprasadpabboju@gmail.com>
2026-04-04 23:36:07 +0530

    Create 198-HouseRobber

---
+++ 198-HouseRobber/198-HouseRobber.java
@@ -0,0 +1,15 @@
+// Last updated: 04/04/2026, 23:36:05
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n==1) return nums[0];
+        int prev2 = nums[0];
+        int prev1 = Math.max(nums[0],nums[1]);
+        for(int i=2;i<n;i++) {
+            int curr = Math.max(nums[i]+prev2,prev1);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+}
