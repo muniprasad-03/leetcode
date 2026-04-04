ca4bd2b7e6859b11752424bf553d258efd790cdf
muniprasad-03 <muniprasadpabboju@gmail.com>
2026-04-04 23:31:13 +0530

    Create 1716-MaximumNonNegativeProductInAMatrix

---
+++ 1716-MaximumNonNegativeProductInAMatrix/1716-MaximumNonNegativeProductInAMatrix.java
@@ -0,0 +1,33 @@
+// Last updated: 04/04/2026, 23:31:10
+class Solution {
+    public int maxProductPath(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        long[][] max = new long[m][n];
+        long[][] min = new long[m][n];
+        max[0][0] = min[0][0] = grid[0][0];
+
+        for(int i=1;i<n;i++) {
+            max[0][i] = min[0][i] = max[0][i-1] * grid[0][i];
+        }
+
+        for(int i=1;i<m;i++) {
+            max[i][0] = min[i][0] = max[i-1][0] * grid[i][0];
+        }
+
+        for(int i=1;i<m;i++) {
+            for(int j=1;j<n;j++) {
+
+                long a = max[i-1][j] * grid[i][j];
+                long b = max[i][j-1] * grid[i][j];
+                long c = min[i-1][j] * grid[i][j];
+                long d = min[i][j-1] * grid[i][j];
+
+                max[i][j] = Math.max(Math.max(a,b), Math.max(c, d));
+                min[i][j] = Math.min(Math.min(a,b), Math.min(c, d));
+            }
+        }
+        long res = max[m-1][n-1];
+        return res < 0 ? -1 : (int)(res % 1000000007);
+    }
+}
