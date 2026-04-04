9a05eb359cb2fa3366297ae6a04c041b2788c271
muniprasad-03 <muniprasadpabboju@gmail.com>
2026-04-04 23:34:52 +0530

    Create 357-CountNumbersWithUniqueDigits

---
+++ 357-CountNumbersWithUniqueDigits/357-CountNumbersWithUniqueDigits.java
@@ -0,0 +1,15 @@
+// Last updated: 04/04/2026, 23:34:50
+class Solution {
+    public int countNumbersWithUniqueDigits(int n) {
+        if(n==0) return 1;
+        int ans = 10;
+        int unqNums = 9;
+        int avbDigits = 9;
+        for(int i=1;i<n;i++){
+            unqNums *= avbDigits;
+            ans += unqNums;
+            avbDigits--;
+        }
+        return ans;
+    }
+}
