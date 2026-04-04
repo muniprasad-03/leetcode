706477ce3d7666f1c0c1adbaea2de5152b8f497f
muniprasad-03 <muniprasadpabboju@gmail.com>
2026-04-04 23:33:42 +0530

    Create 611-ValidTriangleNumber

---
+++ 611-ValidTriangleNumber/611-ValidTriangleNumber.cpp
@@ -0,0 +1,22 @@
+// Last updated: 04/04/2026, 23:33:39
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        sort(nums.begin(),nums.end());
+        for(int k=n-1;k>=2;k--) {
+            int i=0;
+            int j=k-1;
+            while(i<j) {
+                if(nums[i]+nums[j]>nums[k]) {
+                    ans += (j-i);
+                    j--;
+                } else {
+                    i++;
+                }
+            }
+        }
+        return ans;
+    }
+};
