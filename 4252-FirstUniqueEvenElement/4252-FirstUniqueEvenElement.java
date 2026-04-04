bd2af44ae85785de1b802e32c5b8b2e2039ef645
muniprasad-03 <muniprasadpabboju@gmail.com>
2026-04-04 23:26:10 +0530

    Create 4252-FirstUniqueEvenElement

---
+++ 4252-FirstUniqueEvenElement/4252-FirstUniqueEvenElement.java
@@ -0,0 +1,13 @@
+// Last updated: 04/04/2026, 23:26:08
+class Solution {
+    public int firstUniqueEven(int[] nums) {
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        for(int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0)+1);
+        }
+        for(int num : nums) {
+            if(num % 2 == 0 && freq.get(num) == 1) return num;
+        }
+        return -1;
+    }
+}
