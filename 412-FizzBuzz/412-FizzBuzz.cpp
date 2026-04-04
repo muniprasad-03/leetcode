5227ed07a758f0476b1ee2d7fb24503da85991d7
muniprasad-03 <muniprasadpabboju@gmail.com>
2026-04-04 23:34:34 +0530

    Create 412-FizzBuzz

---
+++ 412-FizzBuzz/412-FizzBuzz.cpp
@@ -0,0 +1,19 @@
+// Last updated: 04/04/2026, 23:34:32
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> ans(n);
+        for(int i=1;i<=n;i++) {
+            if(i%15==0) {
+                ans[i-1] = "FizzBuzz";
+            } else if (i%3==0) {
+                ans[i-1] = "Fizz";
+            } else if(i%5==0) {
+                ans[i-1] = "Buzz";
+            } else {
+                ans[i-1] = to_string(i);
+            }
+        }
+        return ans;
+    }
+};
