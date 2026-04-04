3eb4560b11684a74738fe62a0ee113ecdbe14587
muniprasad-03 <muniprasadpabboju@gmail.com>
2026-04-04 23:38:59 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.cpp
@@ -0,0 +1,36 @@
+// Last updated: 04/04/2026, 23:38:56
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char ch:s)
+        {
+            if(ch=='{' || ch=='[' || ch=='(')
+            {
+                st.push(ch);
+            }
+            else
+            {
+                if(st.empty()) return false;
+                if(st.top()=='{' && ch=='}')
+                {
+                    st.pop();
+                }
+                else if(st.top()=='[' && ch==']')
+                {
+                    st.pop();
+                }
+                else if(st.top()=='(' && ch==')')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        if(st.empty()) return true;
+        else return false;
+    }
+};
