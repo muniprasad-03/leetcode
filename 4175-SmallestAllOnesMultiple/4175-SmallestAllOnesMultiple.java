eb375566968750cd1547d34a4cee4e53669d5b87
muniprasad-03 <muniprasadpabboju@gmail.com>
2026-04-04 23:26:49 +0530

    Create 4175-SmallestAllOnesMultiple

---
+++ 4175-SmallestAllOnesMultiple/4175-SmallestAllOnesMultiple.java
@@ -0,0 +1,12 @@
+// Last updated: 04/04/2026, 23:26:47
+class Solution {
+    public int minAllOneMultiple(int k) {
+        if(k%2 == 0 || k%5 == 0) return -1;
+        int rem = 0;
+        for(int len = 1;len <= k; len++) {
+            rem = (rem*10+1) % k;
+            if(rem==0) return len;
+        }
+        return -1;
+    }
+}
