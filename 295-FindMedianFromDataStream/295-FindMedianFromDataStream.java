3cc76c761f2e56c77a0eddcd6502dffcd1a63226
muniprasad-03 <muniprasadpabboju@gmail.com>
2026-04-04 23:35:07 +0530

    Create 295-FindMedianFromDataStream

---
+++ 295-FindMedianFromDataStream/295-FindMedianFromDataStream.java
@@ -0,0 +1,31 @@
+// Last updated: 04/04/2026, 23:35:05
+class MedianFinder {
+    PriorityQueue<Integer> maxHeap;
+    PriorityQueue<Integer> minHeap;
+    public MedianFinder() {
+        maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        minHeap = new PriorityQueue<>();
+    }
+    
+    public void addNum(int num) {
+        maxHeap.offer(num);
+        minHeap.offer(maxHeap.poll());
+        if(minHeap.size() > maxHeap.size()) {
+            maxHeap.offer(minHeap.poll());
+        }
+    }
+    
+    public double findMedian() {
+        if(maxHeap.size() == minHeap.size()) {
+            return (maxHeap.peek()+minHeap.peek())/2.0;
+        }
+        return maxHeap.peek();
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
