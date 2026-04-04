75d1630fdd27bfbdbe073c5e89314eaafe0c6daa
muniprasad-03 <muniprasadpabboju@gmail.com>
2026-04-04 23:35:41 +0530

    Create 225-ImplementStackUsingQueues

---
+++ 225-ImplementStackUsingQueues/225-ImplementStackUsingQueues.java
@@ -0,0 +1,34 @@
+// Last updated: 04/04/2026, 23:35:39
+class MyStack {
+    private Queue<Integer> q1;
+    private Queue<Integer> q2;
+
+    public MyStack() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        while (!q1.isEmpty()) {
+            q2.offer(q1.peek());
+            q1.poll();
+        }
+        q1.offer(x);
+        while (!q2.isEmpty()) {
+            q1.offer(q2.peek());
+            q2.poll();
+        }
+    }
+    
+    public int pop() {
+        return q1.poll();
+    }
+    
+    public int top() {
+        return q1.peek();
+    }
+    
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
