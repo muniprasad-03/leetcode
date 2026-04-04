a0db23a7af5c28d24d0edd8bdba69179b3dec862
muniprasad-03 <muniprasadpabboju@gmail.com>
2026-04-04 23:35:00 +0530

    Create 334-IncreasingTripletSubsequence

---
+++ 334-IncreasingTripletSubsequence/334-IncreasingTripletSubsequence.java
@@ -0,0 +1,17 @@
+// Last updated: 04/04/2026, 23:34:58
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+        for(int num : nums) {
+            if(num <= first) {
+                first = num;
+            } else if(num <= second) {
+                second = num;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
