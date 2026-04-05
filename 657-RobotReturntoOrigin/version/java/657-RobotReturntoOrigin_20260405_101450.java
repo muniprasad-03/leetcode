722f2abeb697d39b655da580696524d5f9827b1a
muniprasad-03 <muniprasadpabboju@gmail.com>
2026-04-05 10:14:53 +0530

    Create 657-RobotReturntoOrigin

---
+++ 657-RobotReturntoOrigin/version/java/657-RobotReturntoOrigin_20260405_101450.java
@@ -0,0 +1,14 @@
+// Last updated: 05/04/2026, 10:14:50
+1class Solution {
+2    public boolean judgeCircle(String moves) {
+3        int upDown = 0;
+4        int leftRight = 0;
+5        for(char ch : moves.toCharArray()) {
+6            if(ch == 'U') upDown++;
+7            else if(ch == 'D') upDown--;
+8            else if(ch == 'L') leftRight++;
+9            else leftRight--;
+10        }
+11        return upDown == 0 && leftRight == 0;
+12    }
+13}
