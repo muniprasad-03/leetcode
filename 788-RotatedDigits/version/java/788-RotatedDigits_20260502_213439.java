23405ac7ce5e645afef56fc435fe6bd0c907f553
muniprasad-03 <muniprasadpabboju@gmail.com>
2026-05-02 21:34:46 +0530

    Create 788-RotatedDigits

---
+++ 788-RotatedDigits/version/java/788-RotatedDigits_20260502_213439.java
@@ -0,0 +1,22 @@
+// Last updated: 02/05/2026, 21:34:39
+1class Solution {
+2    private boolean isGood(int num) {
+3        boolean changed = false;
+4        while(num > 0) {
+5            int d = num % 10;
+6            if(d == 3 || d == 4 || d == 7) return false;
+7            if(d == 2 || d == 5 || d == 6 || d == 9) {
+8                changed = true;
+9            }
+10            num /= 10;
+11        }
+12        return changed;
+13    }
+14    public int rotatedDigits(int n) {
+15        int count = 0;
+16        for(int i = 1; i <= n; i++) {
+17            if(isGood(i)) count++;
+18        }
+19        return count;
+20    }
+21}
