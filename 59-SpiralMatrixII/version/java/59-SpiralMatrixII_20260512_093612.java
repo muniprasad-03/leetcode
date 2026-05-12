dabaa270bb491fa7d553ca2e3ef77535026c59ad
muniprasad-03 <muniprasadpabboju@gmail.com>
2026-05-12 09:36:15 +0530

    Create 59-SpiralMatrixII

---
+++ 59-SpiralMatrixII/version/java/59-SpiralMatrixII_20260512_093612.java
@@ -0,0 +1,21 @@
+// Last updated: 12/05/2026, 09:36:12
+1class Solution {
+2    public int[][] generateMatrix(int n) {
+3        int[][] matrix = new int[n][n];
+4        int[] dirs = new int[]{0, 1, 0, -1, 0};
+5        int row = 0, col = 0, dir = 0;
+6        for (int num=1;num <= n*n;num++) {
+7            matrix[row][col] = num;
+8            int nextRow = row + dirs[dir];
+9            int nextCol = col + dirs[dir+1];
+10            if (nextRow < 0 || nextRow >= n ||
+11                nextCol < 0 || nextCol >= n ||
+12                matrix[nextRow][nextCol] != 0) {
+13                dir = (dir + 1) % 4;
+14            }
+15            row += dirs[dir];
+16            col += dirs[dir+1];
+17        }
+18        return matrix;
+19    }
+20}
