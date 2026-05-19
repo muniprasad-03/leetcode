505c447bebe9f47b115692392ecaf8c9221a9db2
muniprasad-03 <muniprasadpabboju@gmail.com>
2026-05-19 16:11:01 +0530

    Create 2540-MinimumCommonValue

---
+++ 2540-MinimumCommonValue/version/java/2540-MinimumCommonValue_20260519_161052.java
@@ -0,0 +1,10 @@
+// Last updated: 19/05/2026, 16:10:52
+1class Solution {
+2    public int getCommon(int[] nums1, int[] nums2) {
+3        for(int num : nums1) {
+4            int idx = Arrays.binarySearch(nums2, num);
+5            if(idx >= 0) return num; 
+6        }
+7        return -1;
+8    }
+9}
