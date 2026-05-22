9010cae5c914a1ae043ae6e39620fe2066165d10
muniprasad-03 <muniprasadpabboju@gmail.com>
2026-05-22 22:44:21 +0530

    Create 130-SurroundedRegions

---
+++ 130-SurroundedRegions/version/java/130-SurroundedRegions_20260522_224409.java
@@ -0,0 +1,51 @@
+// Last updated: 22/05/2026, 22:44:09
+class Solution {
+    public void solve(char[][] board) {
+        for (int row = 0; row < board.length; row++) {
+            if (board[row][0] == 'O') {
+                dfs(row, 0, board);
+            }
+
+            if (board[row][board[0].length - 1] == 'O') {
+                dfs(row, board[0].length - 1, board);
+            }
+        }
+        for (int col = 0; col < board[0].length; col++) {
+            if (board[0][col] == 'O') {
+                dfs(0, col, board);
+            }
+
+            if (board[board.length - 1][col] == 'O') {
+                dfs(board.length - 1, col, board);
+            }
+        }
+        traverseAndMark(board);
+    }
+
+    private void dfs(int row, int col, char[][] board) {
+        if (row < 0 || col < 0 || row >= board.length || col >= board[0].length) {
+            return;
+        }
+        if (board[row][col] != 'O') {
+            return;
+        }
+        board[row][col] = 'T';
+        dfs(row - 1, col, board);
+        dfs(row + 1, col, board);
+        dfs(row, col + 1, board);
+        dfs(row, col - 1, board);
+    }
+
+    private void traverseAndMark(char[][] board) {
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[0].length; col++) {
+                if (board[row][col] == 'O') {
+                    board[row][col] = 'X';
+                }
+                if (board[row][col] == 'T') {
+                    board[row][col] = 'O';
+                }
+            }
+        }
+    }
+}
